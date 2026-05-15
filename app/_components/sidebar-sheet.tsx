a61e47cbb0225925ff8f1cf15909fdda4d86c2a4
Johan Garcia <johangarciadeoliveira123@outlook.com.br>
2026-05-15 15:57:07 -0300

    feat: add `SidebarSheet` component

---
+++ app/_components/sidebar-sheet.tsx
@@ -0,0 +1,74 @@
+import { Button } from "./ui/button"
+import { Calendar1Icon, HomeIcon, LogOutIcon } from "lucide-react"
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
+import { quickSearchOptions } from "../_constants/search"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import Link from "next/link"
+import Image from "next/image"
+
+const SidebarSheet = () => {
+  return (
+    <SheetContent className="overflow-y-auto px-6">
+      <SheetHeader className="px-0">
+        <SheetTitle className="text-left">Menu</SheetTitle>
+      </SheetHeader>
+
+      <div className="flex items-center gap-3 border-b border-solid py-5">
+        <Avatar className="h-[45px] w-[45px] overflow-hidden border-2 border-solid border-primary">
+          <AvatarImage
+            className="scale-150 object-scale-down"
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fHww"
+          />
+        </Avatar>
+
+        <div>
+          <p className="font-bold">Johan Garcia</p>
+          <p className="text-xs">johangarciadeoliveira123@outlook.com.br</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2 p-5" variant={"ghost"} asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Início
+            </Link>
+          </Button>
+        </SheetClose>
+
+        <Button className="justify-start gap-2 p-5" variant="ghost">
+          <Calendar1Icon size={18} />
+          Agendamentos
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        {quickSearchOptions.map((option) => (
+          <Button
+            key={option.title}
+            className="justify-start gap-2 p-5"
+            variant={"ghost"}
+          >
+            <Image
+              alt={option.title}
+              src={option.imageUrl}
+              width={18}
+              height={18}
+            />
+            {option.title}
+          </Button>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        <Button className="justify-start gap-2 p-5" variant="ghost">
+          <LogOutIcon size={18} />
+          Sair da conta
+        </Button>
+      </div>
+    </SheetContent>
+  )
+}
+
+export default SidebarSheet
