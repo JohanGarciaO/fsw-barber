5323a9b7da02c366cc5ac81b70ede4d6c88f9604
Johan Garcia <johangarciadeoliveira123@outlook.com.br>
2026-05-18 23:24:29 -0300

    fix: change Prisma import in `seed.ts` to use `Common.js`

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client"
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { PrismaClient } = require("@prisma/client")
 
 const prisma = new PrismaClient()
 
